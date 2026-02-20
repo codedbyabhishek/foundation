a6e7ac66d5ed0be028344db3936bd3e940a8b9c1
Abhizerk <vtu16139@veltech.edu.in>
2026-02-20 21:42:38 +0530

    Total average of 3 number

---
+++ total_average.py
@@ -0,0 +1,6 @@
+a = float(input("Enter the first number: "))
+b = float(input("Enter the second number: "))
+c = float(input("Enter the third number: "))
+
+print("The average of the three numbers is: ", (a + b + c) / 3)
+print("The sum of the three numbers is: ", a + b + c)
--- variable.py
+++ variable.py
@@ -2,4 +2,6 @@ name = "Abhishek"
 age = 22
 height = 5.8
 
-print("Helllo " + name + " Guess Your age is " + str(age) + " and your height is " + str(height))
+print(name)
+print(age)
+print(height)
